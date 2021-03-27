4ec4f481a262bcf9c656f75d3ba77b2d4eee287d
naderlikeladder <nader.khalil@gmail.com>
2021-03-27 13:52:06 -0700

    Updates

---
+++ precache-manifest.85505fbe04919d1e869b3e7669cc091a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ede943c07790e2ef102aa71bf735a74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9cb05e27a39f6636167",
+    "url": "./static/css/main.051f19fe.chunk.css"
+  },
+  {
+    "revision": "84b0c87c4c97ee2e889a",
+    "url": "./static/js/2.37ebbd5b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.37ebbd5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9cb05e27a39f6636167",
+    "url": "./static/js/main.6e76584b.chunk.js"
+  },
+  {
+    "revision": "6cfbfd742b23d3ac663e",
+    "url": "./static/js/runtime-main.5f8e3f92.js"
+  }
+]);
